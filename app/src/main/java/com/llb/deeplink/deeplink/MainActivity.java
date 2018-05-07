4f9a8b3e366bba8050ad74c55437db146ca769d1
robinluo <695863436@qq.com>
2018-05-07 22:32:24 +0800

    修改deeplink 和添加静音逻辑

--- app/src/main/java/com/llb/deeplink/deeplink/MainActivity.java
+++ app/src/main/java/com/llb/deeplink/deeplink/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ListViewCompat;
+import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -14,6 +15,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.net.URISyntaxException;
 
@@ -21,30 +23,29 @@ public class MainActivity extends AppCompatActivity {
 
     ListView listView ;
 
+    public static String TAG = "testMainDp";
+
     public Button btnStart;
 
     public Button btnStop;
 
 
-    public static  String[] title = { "波波视频1", "汽车之家1", "爱奇艺", "手机百度",
-            "天天快报", "PPTV" };
+    public static  String[] title = { "PPTV", "腾讯视频", "腾讯新闻", "西瓜视频",
+            "火山小视频", "抖音" };
 
-    public static  String[] dp = {"bobo://bb.web/v?vid=6329306112713555968&pg=jg&pg_position=jg3&partner_source=channel",
-    "autohome://article/platform?newsid=2223180&type=1&showcommentlist=0&label=jixin&ltype=69&dt=20180312",
-    "iqiyi://mobile/home?ftype=27&subtype=347",
-    "baiduboxapp://utils?action=sendIntent&minver=7.4&params=%7B%22intent%22%3A%22intent%3A%23Intent%3Baction%3Dcom.baidu.searchbox.action.HOME%3BS.targetCommand%3D%257B%2522mode%2522%253A%25220%2522%252C%2522intent%2522%253A%2522intent%253A%2523Intent%253BB.bdsb_append_param%253Dtrue%253BS.bdsb_light_start_url%253Dhttp%25253A%25252F%25252Fbaijiahao.baidu.com%25252Fs%25253Fid%25253D1595361518306467257%253Bend%2522%252C%2522class%2522%253A%2522com.baidu.searchbox.xsearch.UserSubscribeCenterActivity%2522%252C%2522min_v%2522%253A%252216787968%2522%257D%3Bend%22%7D&needlog=1&logargs=%7B%22source%22%3A%221021229k%22%2C%22from%22%3A%22openbox%22%2C%22page%22%3A%22other%22%2C%22type%22%3A%22%22%2C%22value%22%3A%22url%22%2C%22channel%22%3A%22%22%2C%22ext%22%3A%22%7B%5C%22sid%5C%22%3A%5C%22%7Bqueryid%7D%5C%22%7D%22%7D",
-    "qnreading://article_detail?nm=20180216A0PRC900&from=aaa6",
-    "pptv://page/player/halfscreen?type=vod&vid=27706889&sid=9044950&utm=194"};
+    public static  String[] dp = {
+    "pptv://page/player/halfscreen?type=vod&vid=27757231&sid=9045060&utm=194",
+    "tenvideo2://?action=1&cover_id=tr1mxts1ncwzfaq&video_id=t0026b9nxmn&from=30125|7",
+    "qqnews://article_9527?nm=20180428C1ONTJ00&from=dkl2",
+    "snssdk32://home/news?gd_label=click_schema_hxhg9",
+    "snssdk1112://main?&push_id=80400050&gd_label=click_schema_hxhg9",
+    "snssdk1128://detail?id=6549850631253789966&gd_label=click_schema_hxhg9"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("snssdk143://detail?groupid=6534762826882351629&gd_label=click_schema_hxhg16"));
-//        //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//        startActivity(intent);
-        // startService(new Intent(this, MyService.class));
         listView = (ListView) findViewById(R.id.list_view);
 
         btnStart = (Button) findViewById(R.id.start);
@@ -76,48 +77,16 @@ public class MainActivity extends AppCompatActivity {
                 String str = dp[i];
                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(str));
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
+                try{
+                    startActivity(intent);
+                }catch (Exception e){
+                    Toast.makeText(getApplicationContext(), "没有安装 " + title[i], Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+
             }
         });
 
-//        web_main = (WebView) findViewById(R.id.web_main);
-//
-//        web_main.loadUrl("file:///android_asset/h5.html");
-//
-//        web_main.setWebViewClient(new WebViewClient() {
-//            @Override
-//            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-//                if (url.startsWith("http") || url.startsWith("https") || url.startsWith("ftp")) {
-//                    //不处理http, https, ftp的请求
-//                    return false;
-//                }
-//                Intent intent;
-//                try {
-//                    intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME);
-//                } catch (URISyntaxException e) {
-//                    // Log.e(TAG, "URISyntaxException: " + e.getLocalizedMessage());
-//                    return false;
-//                }
-//                intent.setComponent(null);
-//                try {
-//                    startActivity(intent);
-//                } catch (ActivityNotFoundException e) {
-//                    //XLLog.e(TAG, "ActivityNotFoundException: " + e.getLocalizedMessage());
-//                    return false;
-//                }
-//                return true;
-//            }
-//            //            @Override
-////            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-////
-////                if (url.startsWith("will://")) {
-////                    Uri uri = Uri.parse(url);
-////                    Log.e("---------scheme: ", uri.getScheme() + "host: " + uri.getHost() + "Id: " + uri.getPathSegments().get(0));
-////                    Toast.makeText(MainActivity.this, "打开新的页面", Toast.LENGTH_LONG).show();
-////                    return true; //返回true，代表要拦截这个url
-////                }
-////                return super.shouldOverrideUrlLoading(view, url);
-////            }
-//        });
     }
+
 }
--- app/src/main/java/com/llb/deeplink/deeplink/MyService.java
+++ app/src/main/java/com/llb/deeplink/deeplink/MyService.java
@@ -1,83 +1,89 @@
 package com.llb.deeplink.deeplink;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.IBinder;
-import android.support.annotation.IntDef;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import static com.llb.deeplink.deeplink.MainActivity.title;
 
 public class MyService extends Service {
-    WebView mWebView ;
-    View root ;
+    AudioManager mAudioManager;
+    int index;
 
     private int i = 0;
 
-    private void initView(){
-        root = LayoutInflater.from(this).inflate(R.layout.activity_js,
-                null);
-        mWebView = root.findViewById(R.id.webview);
-    }
-
     Handler handler = new Handler();
-
     Runnable runnable=new Runnable(){
         @Override
         public void run() {
 
             if (i >= MainActivity.dp.length){
+                try {
+                    Thread.sleep(60 * 1000);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, index);
+                startActivity(new Intent(MyService.this, MainActivity.class));
                 return;
             }
 
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(MainActivity.dp[i++]));
+            String Strtitle = MainActivity.title[i];
+            Log.d(MainActivity.TAG, "i : " + i);
+            Log.d(MainActivity.TAG, "title : " + MainActivity.title[i]);
+            Log.d(MainActivity.TAG, "deeplink : " + MainActivity.dp[i]);
+
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(MainActivity.dp[i]));
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
+            try {
+                startActivity(intent);
+
+                if (Strtitle.contains("视频") || Strtitle.contains("TV")
+                        || Strtitle.contains("tv")){
+                    mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, 0);
+                }
+//                else{
+//                    mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, index, 0);
+//                    Log.d(MainActivity.TAG, "setStreamVolume : " + index);
+//                }
+
+            }catch (Exception e){
+                Toast.makeText(getApplicationContext(), "没有安装 " + title[i], Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+
             // TODO Auto-generated method stub
             //要做的事情，这里再次调用此Runnable对象，以实现每两秒实现一次的定时器操作
-
-            handler.postDelayed(this, 2000);
+            i++;
+            handler.postDelayed(runnable, 60 * 1000);
 
         }
     };
 
     @Override
+    public void onStart(Intent intent, int startId) {
+        i = 0;
+        handler.removeCallbacks(runnable);
+        handler.postDelayed(runnable, 2000);
+        index = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        Log.d(MainActivity.TAG, "on start index : " + index);
+        super.onStart(intent, startId);
+    }
+
+    @Override
     public void onCreate() {
         super.onCreate();
-        i = 0;
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("snssdk143://detail?groupid=6534762826882351629&gd_label=click_schema_hxhg16"));
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
 
-        handler.postDelayed(runnable, 2000);
-
-//        initView();
-//        WebSettings webSettings = mWebView.getSettings();
-//
-//        // 设置与Js交互的权限
-//        webSettings.setJavaScriptEnabled(true);
-//        // 设置允许JS弹窗
-//        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-//
-//        // 先载入JS代码
-//        // 格式规定为:file:///android_asset/文件名.html
-//        mWebView.loadUrl("file:///android_asset/javascript.html");
-//
-//
-//        mWebView.postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//
-//                // 注意调用的JS方法名要对应上
-//                // 调用javascript的callJS()方法
-//                mWebView.loadUrl("javascript:callJS()");
-//            }
-//        }, 1000);
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
     }
 
     @Override
